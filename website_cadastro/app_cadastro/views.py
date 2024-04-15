a57d6090bbd603fa2335e69a21c0c365e9598586
Melao12125 <MatheusForte12125@gmail.com>
2024-04-15 20:58:04 -0300

    update

--- website_cadastro/app_cadastro/views.py
+++ website_cadastro/app_cadastro/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    print(request)
     return render(request, 'home/partial/register.html')
--- website_cadastro/website_cadastro/settings.py
+++ website_cadastro/website_cadastro/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'website_cadastro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
